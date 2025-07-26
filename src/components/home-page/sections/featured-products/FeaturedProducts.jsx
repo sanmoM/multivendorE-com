0c856f190200702b06d8334770a1d303b528a2c0
anmobd111 <sanmobd111@gmail.com>
2025-07-26 15:39:51 +0600

    home page done

--- src/components/home-page/sections/featured-products/FeaturedProducts.jsx
+++ src/components/home-page/sections/featured-products/FeaturedProducts.jsx
@@ -1,20 +1,44 @@
+"use client";
+
 import SectionTitle from '@/components/shared/section-title/SectionTitle'
 import { products } from '@/data'
 import FeaturedProductCard from '../../components/product-card/ProductCard'
 import LoadMoreBtn from '@/components/shared/load-more-btn/LoadMoreBtn'
+import { useEffect, useState } from 'react';
 
 export default function FeaturedProducts() {
+    // product quantity logic
+    const [visibleProducts, setVisibleProducts] = useState(products);
+
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth < 1024) {
+                // Show only 3 products on small devices
+                setVisibleProducts(products.slice(0, 3));
+            } else {
+                // Show all products on large devices
+                setVisibleProducts(products);
+            }
+        };
+
+        // Initial check
+        handleResize();
+
+        // Listen to resize
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
     return (
         <div>
             <SectionTitle title='Featured Products' />
             <div className='grid grid-cols-3 lg:grid-cols-5 gap-x-4 gap-y-8 mt-4 lg:mt-6'>
                 {
-                    products.map((product) => (
+                    visibleProducts.map((product) => (
                         <FeaturedProductCard key={product.id} item={product} />
                     ))
                 }
             </div>
-            <LoadMoreBtn className='mt-8' />
+            <LoadMoreBtn className='mt-6' />
         </div>
     )
 }
--- src/components/home-page/sections/more-featured-product/MoreFeaturedProducts.jsx
+++ src/components/home-page/sections/more-featured-product/MoreFeaturedProducts.jsx
@@ -1,20 +1,44 @@
-import SectionTitle from '@/components/shared/section-title/SectionTitle'
-import { products } from '@/data'
-import LoadMoreBtn from '@/components/shared/load-more-btn/LoadMoreBtn'
-import FeaturedProductCard from '../../components/product-card/ProductCard'
+"use client";
+
+import LoadMoreBtn from '@/components/shared/load-more-btn/LoadMoreBtn';
+import SectionTitle from '@/components/shared/section-title/SectionTitle';
+import { products } from '@/data';
+import { useEffect, useState } from 'react';
+import FeaturedProductCard from '../../components/product-card/ProductCard';
 
 export default function MoreFeaturedProducts() {
+    // product quantity logic
+    const [visibleProducts, setVisibleProducts] = useState(products);
+
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth < 1024) {
+                // Show only 3 products on small devices
+                setVisibleProducts(products.slice(0, 3));
+            } else {
+                // Show all products on large devices
+                setVisibleProducts(products);
+            }
+        };
+
+        // Initial check
+        handleResize();
+
+        // Listen to resize
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
     return (
         <div>
             <SectionTitle title='More Featured Products' />
             <div className='grid grid-cols-3 lg:grid-cols-5 gap-x-4 gap-y-8 mt-4 lg:mt-6'>
                 {
-                    products.map((product) => (
+                    visibleProducts.map((product) => (
                         <FeaturedProductCard key={product.id} item={product} />
                     ))
                 }
             </div>
-            <LoadMoreBtn className='mt-8' />
+            <LoadMoreBtn className='mt-6' />
         </div>
     )
 }
