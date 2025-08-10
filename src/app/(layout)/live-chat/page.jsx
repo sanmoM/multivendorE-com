88f768aa76276767b11d68b0ce51a7dc360e84c6
anmobd111 <sanmobd111@gmail.com>
2025-08-10 17:52:38 +0600

    remove un used code

--- src/app/(layout)/live-chat/page.jsx
+++ src/app/(layout)/live-chat/page.jsx
@@ -13,59 +13,12 @@ export default function ChatInterface() {
                 <header className="bg-tertiary text-white p-4 rounded-t-2xl">
                     <div className="max-w-4xl mx-auto flex items-center justify-between">
                         <div className="flex items-center space-x-4">
-                            {/* <Image
-                                src="https://res.cloudinary.com/djv4xa6wu/image/upload/v1735722165/AbhirajK/Abhirajk.webp"
-                                alt="Profile"
-                                className="w-10 h-10 rounded-full object-cover border-2 border-white"
-                                width={40}
-                                height={40}
-                            /> */}
                             <Logo className={"w-10 h-10 text-primary"} />
                             <div>
                                 <h1 className="font-bold text-primary">Live Chat</h1>
-                                {/* <span
-                                href="https://abhirajk.vercel.app"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                className="text-sm text-blue-100 hover:text-white transition"
-                            >
-                                Visit Portfolio →
-                            </span> */}
                             </div>
                         </div>
                         <div className="flex items-center space-x-4">
-                            {/* <button className="p-2 hover:bg-secondary/30 rounded-full transition" type="button" aria-label="Phone">
-                            <svg
-                                className="w-5 h-5"
-                                fill="none"
-                                stroke="#856161"
-                                viewBox="0 0 24 24"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth={2}
-                                    d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
-                                />
-                            </svg>
-                        </button>
-                        <button className="p-2 hover:bg-secondary/30 rounded-full transition" type="button" aria-label="Video">
-                            <svg
-                                className="w-5 h-5"
-                                fill="none"
-                                stroke="#856161"
-                                viewBox="0 0 24 24"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth={2}
-                                    d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z"
-                                />
-                            </svg>
-                        </button> */}
                             <button className="p-2 hover:bg-secondary/30 rounded-full transition" type="button" aria-label="More options">
                                 <svg
                                     className="w-5 h-5"
