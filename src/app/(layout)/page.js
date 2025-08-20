34a3c917126cf5fee28f4ed9066a8032c2cb82d4
anmobd111 <sanmobd111@gmail.com>
2025-08-20 10:06:21 +0600

    change promotion api

--- src/app/(layout)/page.js
+++ src/app/(layout)/page.js
@@ -7,7 +7,6 @@ import PromotionalSection from "@/components/home-page/sections/promotional-sect
 import MobileHeaderWithSearchbar from "@/components/root-layout/header/components/mobile-header-withSearchbar/MobileHeaderWithSearchbar";
 import Container from "@/components/shared/container/Container";
 import FeaturedItems from "@/components/shared/featured-items/FeaturedItems";
-import Items from "@/components/shared/items/Items";
 import PrimaryCard from "@/components/shared/primary-card/PrimaryCard";
 import { products } from "@/data";
 import { getData } from "@/utils/fetch";
@@ -19,7 +18,7 @@ export default async function Home() {
     <div>
       <MobileHeaderWithSearchbar title={"Home"} />
       <Banner data={res.sliders} />
-      <PromotionalSection data={res.offers} />
+      <PromotionalSection />
       <BrowseSection />
       <Container className={""}>
         <FeaturedItems title={"Featured Products"} desktopView={5} mobileView={3} >
@@ -31,17 +30,6 @@ export default async function Home() {
             ))
           }
         </FeaturedItems>
-        {/* <div>
-          <Items title={"More Featured Products"} desktopView={5} mobileView={3} >
-            {
-              products?.map((product, index) => (
-                <Link href={"/single-item"} className="block" key={index} >
-                  <PrimaryCard item={{ image: product?.image, title: product?.name, subtitle: `$${product?.price}` }} containerClassName={"px-2"} />
-                </Link>
-              ))
-            }
-          </Items>
-        </div> */}
         <MoreFeaturedProducts />
       </Container>
     </div>
--- src/components/home-page/sections/promotional-section/PromotionalSection.jsx
+++ src/components/home-page/sections/promotional-section/PromotionalSection.jsx
@@ -1,26 +1,28 @@
 
-"use client";
 import CustomSlider from "@/components/shared/custom-slider/CustomSlider";
 import { IMAGE_BASE_URL } from "@/config";
+import { getData } from "@/utils/fetch";
 import Image from "next/image";
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
 
-export default function PromotionalSection({ data }) {
+export default async function PromotionalSection() {
+    const { offer } = await getData("/vendor-slider")
+    console.log(offer)
     return (
         <div className="rounded-lg  mt-6 md:mt-8 max-w-[calc(1280px+1rem)] mx-auto hidden lg:block">
             <CustomSlider>
                 {
-                    data.map((item, index) => (
+                    offer?.map((item, index) => (
                         <div key={index} className="px-2 outline-0">
                             <Image
-                                src={IMAGE_BASE_URL + item.product_image}
+                                src={IMAGE_BASE_URL + item?.image}
                                 alt={`Discount ${item}`}
                                 height={400}
                                 width={400}
                                 className="w-full h-auto rounded-lg aspect-[2/1] object-cover"
                             />
-                            <p className="text-primary font-medium mt-4">{`${parseInt(item.discount_percentage)} % off on ${item.category}`}</p>
+                            <p className="text-primary font-medium mt-4">{`${parseInt(item.offer_percentage)} % off on ${item.name}`}</p>
                         </div>
                     ))
                 }
