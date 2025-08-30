e4fef85569149d89274580f57323a96f30221373
anmobd111 <sanmobd111@gmail.com>
2025-08-30 15:31:48 +0600

    button

--- src/components/shared/button/Button.jsx
+++ src/components/shared/button/Button.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function Button({ text, className, onClick, type="button" }) {
     return (
-        <button className={cn("bg-orange-400 text-primary text-sm lg:text-base font-semibold py-2 lg:py-3 px-4 rounded-full", className)} onClick={onClick}>
+        <button className={cn("bg-orange-400 text-primary text-sm lg:text-base font-semibold py-2.5 lg:py-3 px-6 lg:px-8 rounded-full", className)} onClick={onClick}>
             {text}
         </button>
     )
