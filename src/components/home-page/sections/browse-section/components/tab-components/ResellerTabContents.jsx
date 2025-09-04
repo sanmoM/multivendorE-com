aabb09d3a2f93074fa0fb00bd78187a3fff41523
anmobd111 <sanmobd111@gmail.com>
2025-09-04 16:49:44 +0600

    add resellers

--- src/components/home-page/sections/browse-section/components/tab-components/ResellerTabContents.jsx
+++ src/components/home-page/sections/browse-section/components/tab-components/ResellerTabContents.jsx
@@ -1,20 +1,37 @@
-import CustomSlider from '@/components/shared/custom-slider/CustomSlider'
-import Filter from '@/components/shared/filter/Filter'
-import PrimaryCard from '@/components/shared/primary-card/PrimaryCard'
+import CustomSlider from '@/components/shared/custom-slider/CustomSlider';
+import Filter from '@/components/shared/filter/Filter';
+import PrimaryCard from '@/components/shared/primary-card/PrimaryCard';
+import useAxios from '@/hooks/useAxios';
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
-export default function ResellerTabContents({ resellers }) {
+export default function ResellerTabContents() {
     const [isLoading, setIsLoading] = useState(true);
+
+    const [resellers, setResellers] = useState(Array(6).fill(null));
+    const axios = useAxios();
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const res = await axios.get("/all/reseller")
+            setResellers(res?.data?.resellers);
+            console.log(res?.data);
+            setIsLoading(false);
+        };
+        fetchData();
+    }, []);
+
     return (
         <div>
-            <div className='mb-6 px-2 lg:px-0'>
+            {/* <div className='mb-6 px-2 lg:px-0'>
                 <Filter />
-            </div>
+            </div> */}
             <CustomSlider mobileView={3} desktopView={6}>
                 {
-                    resellers?.map((shop) => (
-                        <>
-                            <PrimaryCard key={shop.id} item={shop} containerClassName={"px-2"} />
-                        </>
+                    resellers?.map((item, index) => (
+                        <Link href={`/reseller?id=${item?.reseller?.id}`} className='block w-full'>
+                            <PrimaryCard key={index} item={{ image: item?.reseller?.image, title: item?.reseller?.business_name, subtitle: item?.reseller?.bussiness_address }} containerClassName={"px-2"} isLoading={isLoading} />
+                        </Link>
                     ))
                 }
             </CustomSlider>
--- src/components/shared/primary-card/PrimaryCard.jsx
+++ src/components/shared/primary-card/PrimaryCard.jsx
@@ -1,7 +1,11 @@
+"use client"
+
 import { IMAGE_BASE_URL } from "@/config";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function PrimaryCard({ item, containerClassName, isLoading }) {
+    const [imgSrc, setImgSrc] = useState(IMAGE_BASE_URL + item?.image);
     return (
         <>
             {
@@ -17,7 +21,7 @@ export default function PrimaryCard({ item, containerClassName, isLoading }) {
                         <div className="h-3 bg-gray-200 rounded mt-2 w-1/2 animate-pulse" />
                     </div> :
                     <div className={containerClassName}>
-                        <Image src={IMAGE_BASE_URL + item?.image} alt="product-1" width={300} height={400} className="w-full aspect-square object-cover rounded-lg" />
+                        <Image src={imgSrc} onError={() => setImgSrc("/images/default-image.png")} alt="product-1" width={300} height={400} className="w-full aspect-square object-cover rounded-lg" />
                         <h3 className="text-primary font-medium mt-4 text-sm lg:text-lg">{item?.title?.slice(0, 20)}</h3>
                         <p className="text-secondary mt-1 text-xs lg:text-sm">{item?.subtitle?.length > 30 ? item?.subtitle?.slice(0, 30)?.concat('...') : item?.subtitle}</p>
                     </div>
